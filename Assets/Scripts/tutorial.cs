cd6755da5f8d0190e34cabc28b7fc4dab2f26363
bar-schtalman <barschtalman@gmail.com>
2023-05-17 02:08:15 +0300

    Update tutorial.cs

--- Assets/Scripts/tutorial.cs
+++ Assets/Scripts/tutorial.cs
@@ -2,16 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
 using TMPro;
 
 /*
-    this scripts used to control the tutorial and change/active game parts
+    This script is used to control the tutorial and change/activate game parts.
 */
 
-public class tutorial : MonoBehaviour
+public class Tutorial : MonoBehaviour
 {
-    // Start is called before the first frame update
     public GameObject cables;
     public GameObject txt;
     public GameObject cable_drop;
@@ -25,64 +23,68 @@ public class tutorial : MonoBehaviour
     public bool drop_cable_touch = false;
     public bool cable_touch = false;
     public bool move_start = false;
-    string open_text = "hey new player\nwelcome to your new job\nas window cleaner!";
-    string first_hint = "hurry up!\nclimb the building\nand start cleaning!!";
-    string change_items = "your tools are:\nMop, Sponge and Money bag.\nchange them with 'Z 'X 'C 'V\nkeys";
-    string patienc_bar = "see the patience bar\nat the top left.\nwhen building is dirty\nthe boss is getting mad!";
-    string drop_cable = "see this blue attach point?\nits next to the door\nthis is how you can\nget down from the cables";
-    void Start()
+    private string open_text = "Hey new player\nWelcome to your new job\nas window cleaner!";
+    private string first_hint = "Hurry up!\nClimb the building\nand start cleaning!!";
+    private string change_items = "Your tools are:\nMop, Sponge, and Money bag.\nChange them with 'Z', 'X', 'C', 'V' keys";
+    private string patience_bar = "See the patience bar\nat the top left.\nWhen the building is dirty\nthe boss is getting mad!";
+    private string drop_cable = "See this blue attach point?\nIt's next to the door.\nThis is how you can\ndrop down from the cables.";
+
+    private void Start()
     {
-        tutorial_text.text = open_text; // first text that should be displayed
-        StartCoroutine(first_hint_change());
+        tutorial_text.text = open_text; // First text that should be displayed
+        StartCoroutine(FirstHintChange());
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-
-        if(cable_touch){
+        if (cable_touch)
+        {
             tutorial_text.text = change_items;
-            //tutorial_text.transform.position = new Vector3(344.6f,-215.19f,1.84f);
             cables_arrow.GetComponent<SpriteRenderer>().enabled = false;
-            StartCoroutine(change_to_bar_hint());
-        
+            StartCoroutine(ChangeToBarHint());
+            cable_touch = false;
         }
-        if(drop_cable_touch){
+        if (drop_cable_touch)
+        {
             drop_cable_touch = false;
             cable_drop_arrow.GetComponent<SpriteRenderer>().enabled = false;
             tutorial_text.text = "GOOD LUCK!";
-            StartCoroutine(empty_text());
-            }
-        
-        
+            StartCoroutine(EmptyText());
+        }
     }
-    IEnumerator first_hint_change(){
+
+    private IEnumerator FirstHintChange()
+    {
         yield return new WaitForSeconds(4f);
         cables.SetActive(true);
         cables_arrow.GetComponent<SpriteRenderer>().enabled = true;
         tutorial_text.text = first_hint;
     }
-    IEnumerator change_to_bar_hint(){
+
+    private IEnumerator ChangeToBarHint()
+    {
         yield return new WaitForSeconds(10f);
-        tutorial_text.text = patienc_bar;
-        health_bar.GetComponent<p_bar>().increaseFull();
+        tutorial_text.text = patience_bar;
+        health_bar.GetComponent<P_Bar>().IncreaseFull();
         health_bar.SetActive(true);
         bar_arrow.GetComponent<SpriteRenderer>().enabled = true;
-        StartCoroutine(change_to_drop_cable_hint());
+        StartCoroutine(ChangeToDropCableHint());
     }
-    IEnumerator change_to_drop_cable_hint(){
+
+    private IEnumerator ChangeToDropCableHint()
+    {
         yield return new WaitForSeconds(8f);
-        //cable_drop.GetComponent<CircleCollider2D>().enabled = true;
         bar_arrow.GetComponent<SpriteRenderer>().enabled = false;
         cable_drop_arrow.GetComponent<SpriteRenderer>().enabled = true;
         tutorial_text.text = drop_cable;
         cable_drop.SetActive(true);
     }
-    IEnumerator empty_text(){
+
+    private IEnumerator EmptyText()
+    {
         yield return new WaitForSeconds(4f);
         background.GetComponent<SpriteRenderer>().enabled = false;
         txt.SetActive(false);
-        // tutorial_text.text = " ";
-        health_bar.GetComponent<p_bar>().increaseFull();
+        health_bar.GetComponent<P_Bar>().IncreaseFull();
     }
 }
