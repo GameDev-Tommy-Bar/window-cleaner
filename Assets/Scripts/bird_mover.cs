c06b8484fc73d879bb5e017a6df3113a3c124b23
bar-schtalman <barschtalman@gmail.com>
2023-05-24 19:09:58 +0300

    assets update

---
+++ Assets/Scripts/bird_mover.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bird_mover : MonoBehaviour
+{
+   public float moveSpeed = 3f; // The speed at which the bird moves
+   bool flip = false;
+
+    private void Update()
+    {
+        // Move the bird horizontally
+        if(moveSpeed < 0 ){
+                transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
+            if(flip == false){
+            transform.eulerAngles = new Vector3(0,180,0);
+            flip = true;
+            }
+        }
+        if(moveSpeed > 0 ){
+            transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+            if(flip == true){
+            transform.eulerAngles = new Vector3(0,0,0);
+            flip = false;
+            }
+        }
+    }
+}
