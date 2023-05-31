43220e5beb61b72cc52230a48bc14b692e5813d8
bar-schtalman <barschtalman@gmail.com>
2023-05-31 20:07:17 +0300

    Update get_points.cs

--- Assets/Scripts/get_points.cs
+++ Assets/Scripts/get_points.cs
@@ -7,14 +7,12 @@ public class get_points : MonoBehaviour
 {
     // Start is called before the first frame update
     public TMP_Text scoreText;
+
     void Start()
     {
         scoreText.text = player_stats.score + " coins";
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    void Update() { }
 }
