b09b29fe9428f80f1b039e3f50d5a1548700aaff
bar-schtalman <barschtalman@gmail.com>
2023-06-14 01:53:09 +0300

    Update upgrade_item.cs

--- Assets/Scripts/upgrade_item.cs
+++ Assets/Scripts/upgrade_item.cs
@@ -6,6 +6,7 @@ using TMPro;
 public class upgrade_item : MonoBehaviour
 {
     // this script handle the power ups upgrade shop
+    public float cost_factor = 2f;
     public float amount_to_add = 0.1f;
 
     public void buy_cable_speed()
@@ -14,7 +15,7 @@ public class upgrade_item : MonoBehaviour
         {
             player_stats.cable_speed_add += amount_to_add;
             player_stats.score -= player_stats.speed_cost;
-            player_stats.speed_cost += 2;
+            player_stats.speed_cost += cost_factor;
         }
     }
 
@@ -24,7 +25,7 @@ public class upgrade_item : MonoBehaviour
         {
             player_stats.mud_fade_duration -= amount_to_add;
             player_stats.score -= player_stats.sponge_cost;
-            player_stats.sponge_cost += 2;
+            player_stats.sponge_cost += cost_factor;
         }
     }
 
@@ -36,7 +37,7 @@ public class upgrade_item : MonoBehaviour
         {
             player_stats.bubbles_fade_duration -= amount_to_add;
             player_stats.score -= player_stats.mop_cost;
-            player_stats.mop_cost += 2;
+            player_stats.mop_cost += cost_factor;
         }
     }
 
@@ -46,7 +47,7 @@ public class upgrade_item : MonoBehaviour
         {
             player_stats.dirt_delay += amount_to_add;
             player_stats.score -= player_stats.dirt_delay_cost;
-            player_stats.dirt_delay_cost += 2;
+            player_stats.dirt_delay_cost += cost_factor;
         }
     }
 }
